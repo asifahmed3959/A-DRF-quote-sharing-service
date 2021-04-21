307489a5d3c7bd9d9f474bb0ad26ef481ab6a68f
Asif Ahmed <asif.ahmed.apploye@gmail.com>
2021-04-21 11:12:31 +0600

    network configed

--- quotations/settings.py
+++ quotations/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.129.11.239']
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'accounts.User'
 
